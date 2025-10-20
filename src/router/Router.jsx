e624a7546b09b9ff7391de7f7a6f27ec13623af7
LamCao1130 <caohailam3011@gmail.com>
2025-10-21 00:20:45 +0700

    new commit

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -7,10 +7,18 @@ import ManageClass from "../pages/teacher/pages/ManageClass";
 import HeaderStudent from "../pages/student/components/HeaderStudent";
 import TeacherProtectedRouter from "./TeacherProtectedRouter";
 import Fail403 from "../components/Fail403";
+import RouterPrivate from "./RouterPrivate";
 
 let router = createBrowserRouter([
   { path: "/fail403", element: <Fail403 /> },
-  { path: "/", element: <App /> },
+  {
+    path: "/",
+    element: (
+      <RouterPrivate>
+        <App />
+      </RouterPrivate>
+    ),
+  },
   { path: "/student/homepage", element: <HomePageStudent /> },
   {
     path: "/teacher",
