4901b4f883845abb629e24dc070ff8e4225406fa
LamCao1130 <caohailam3011@gmail.com>
2025-11-10 13:48:08 +0700

    add

--- src/pages/student/components/Listening.jsx
+++ src/pages/student/components/Listening.jsx
@@ -3,13 +3,21 @@ import ExerciseNavBar from "./ExerciseNavBar";
 import HeaderCLassStudent from "./HeaderCLassStudent";
 import { useParams } from "react-router";
 import studentApi from "../studentApi/studentApi";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Modal } from "react-bootstrap";
 import { CiVolumeHigh } from "react-icons/ci";
 
 export default function Listening() {
   const { lessonId } = useParams();
   let [dataListenShort, setDataListenShort] = useState();
   let [dataListenLong, setDataListenLong] = useState();
+  const [answers, setAnswers] = useState([]);
+  let [resultFail, setResultFail] = useState();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   useEffect(() => {
     async function callEx() {
       let dataCall = await studentApi.getListListeningByLessonId(lessonId);
@@ -26,6 +34,43 @@ export default function Listening() {
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = "en-US";
     speechSynthesis.speak(utterance);
+  };
+
+  const handleSelect = (questionId, optionId) => {
+    setAnswers((prev) => {
+      const existing = prev.find((a) => a.questionId === questionId);
+
+      if (existing) {
+        const alreadySelected = existing.selected.includes(optionId);
+        const updatedSelected = alreadySelected
+          ? existing.selected.filter((id) => id !== optionId) // bỏ chọn
+          : [...existing.selected, optionId]; // thêm chọn
+
+        // Cập nhật lại trong mảng
+        return prev.map((a) =>
+          a.questionId === questionId ? { ...a, selected: updatedSelected } : a
+        );
+      } else {
+        // Nếu câu hỏi chưa có trong state thì thêm mới
+        return [...prev, { questionId, selected: [optionId] }];
+      }
+    });
+  };
+
+  const handleTextChange = (questionId, text) => {
+    setAnswers((prev) => {
+      const exist = prev.find((a) => a.questionId === questionId);
+
+      if (exist) {
+        // update phần tử đã có
+        return prev.map((a) =>
+          a.questionId === questionId ? { ...a, textAnswer: text } : a
+        );
+      } else {
+        // thêm mới nếu chưa có
+        return [...prev, { questionId, textAnswer: text }];
+      }
+    });
   };
 
   return (
@@ -51,7 +96,10 @@ export default function Listening() {
                         {q.options.map((op) => {
                           return (
                             <>
-                              <input type="checkbox" />
+                              <input
+                                type="checkbox"
+                                onChange={() => handleSelect(q.id, op.id)}
+                              />
                               <span>{op.optionText}</span>
                               <br />
                             </>
@@ -94,6 +142,9 @@ export default function Listening() {
                             padding: "10px",
                             marginTop: "10px",
                           }}
+                          onChange={(e) =>
+                            handleTextChange(q.id, e.target.value)
+                          }
                         />
                         <br />
                         <br />
@@ -106,6 +157,54 @@ export default function Listening() {
           );
         })}
       </div>
+      <Button
+        style={{ margin: "30px" }}
+        onClick={async () => {
+          console.log(answers);
+          try {
+            let res = await studentApi.getListFailOptionListen(
+              answers,
+              lessonId
+            );
+            console.log(res);
+            setResultFail(res);
+            handleShow();
+          } catch (e) {
+            console.log(e);
+          }
+        }}
+      >
+        Nộp nài
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Những câu sai</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {resultFail?.map((p) => {
+            return (
+              <>
+                <p style={{ color: "red" }}>
+                  {p.questionText},{" "}
+                  <span style={{ color: "green" }}>
+                    answer: {p.textTrueAnswer}
+                  </span>
+                </p>
+              </>
+            );
+          })}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="primary"
+            onClick={() => {
+              handleClose();
+            }}
+          >
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
--- src/pages/student/components/Reading.jsx
+++ src/pages/student/components/Reading.jsx
@@ -1,12 +1,140 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ExerciseNavBar from "./ExerciseNavBar";
 import HeaderCLassStudent from "./HeaderCLassStudent";
+import studentApi from "../studentApi/studentApi";
+import { useParams } from "react-router";
+import { Button, Card, Modal } from "react-bootstrap";
 
 export default function Reading() {
+  const { lessonId } = useParams();
+  let [dataReading, setDataReading] = useState();
+
+  const [answers, setAnswers] = useState([]);
+  let [resultFail, setResultFail] = useState();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    async function callEx() {
+      let dataCall = await studentApi.getListReadingByLessonId(lessonId);
+      console.log(dataCall);
+      setDataReading(dataCall);
+    }
+    callEx();
+  }, []);
+
+  const handleSelect = (questionId, optionId) => {
+    setAnswers((prev) => {
+      const existing = prev.find((a) => a.questionId === questionId);
+
+      if (existing) {
+        const alreadySelected = existing.selected.includes(optionId);
+        const updatedSelected = alreadySelected
+          ? existing.selected.filter((id) => id !== optionId) // bỏ chọn
+          : [...existing.selected, optionId]; // thêm chọn
+
+        // Cập nhật lại trong mảng
+        return prev.map((a) =>
+          a.questionId === questionId ? { ...a, selected: updatedSelected } : a
+        );
+      } else {
+        // Nếu câu hỏi chưa có trong state thì thêm mới
+        return [...prev, { questionId, selected: [optionId] }];
+      }
+    });
+  };
+
   return (
-    <div>
-      <HeaderCLassStudent />
-      <ExerciseNavBar />
-    </div>
+    <>
+      <div>
+        <HeaderCLassStudent />
+        <ExerciseNavBar />
+        {dataReading?.map((item) => {
+          return (
+            <>
+              <Card style={{ marginTop: "30px" }}>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Body>
+                  <Card.Text>{item.passageContent}</Card.Text>
+                  {item.questions.map((q) => {
+                    return (
+                      <>
+                        <p>{q.questionText}</p>
+                        {q.options.map((op) => {
+                          return (
+                            <>
+                              <input
+                                type="checkbox"
+                                name=""
+                                id=""
+                                onChange={() => handleSelect(q.id, op.id)}
+                              />
+                              <span>{op.optionText}</span>
+                              <br />
+                            </>
+                          );
+                        })}
+                        <br />
+                      </>
+                    );
+                  })}
+                </Card.Body>
+              </Card>
+            </>
+          );
+        })}
+      </div>
+      <Button
+        style={{ margin: "30px" }}
+        onClick={async () => {
+          console.log(answers);
+          try {
+            let res = await studentApi.getListFailOptionReading(
+              answers,
+              lessonId
+            );
+            console.log(res);
+            setResultFail(res);
+            handleShow();
+          } catch (e) {
+            console.log(e);
+          }
+        }}
+      >
+        Nộp nài
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Những câu sai</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {resultFail?.map((p) => {
+            return (
+              <>
+                <p style={{ color: "red" }}>
+                  {p.questionText},{" "}
+                  <span style={{ color: "green" }}>
+                    answer: {p.textTrueAnswer}
+                  </span>
+                </p>
+              </>
+            );
+          })}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="primary"
+            onClick={() => {
+              handleClose();
+            }}
+          >
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
